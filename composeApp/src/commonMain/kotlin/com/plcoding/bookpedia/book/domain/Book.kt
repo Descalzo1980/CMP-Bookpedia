7ab308a766a532f4d3161dffcbe7d51aa5a71576
Leonov Stas <ragioniere1980@gmail.com>
2024-11-23 23:12:36 +0300

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
@@ -0,0 +1,15 @@
+package com.plcoding.bookpedia.book.domain
+
+data class Book(
+    val id : String,
+    val title: String,
+    val image: String,
+    val author: List<String>,
+    val description: String?,
+    val language: List<String>,
+    val firstPublishYear: String?,
+    val averageRating: Double?,
+    val ratingCount: Int?,
+    val numPage: Int?,
+    val numEdition: Int?
+)
